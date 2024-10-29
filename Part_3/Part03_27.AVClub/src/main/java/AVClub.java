7838b7170b04632d45dbbb1e39555fec26808d71
Matheus Ricardo <matheusri1@hotmail.com>
2024-10-29 17:55:44 -0300

    Add files via upload

---
+++ Part_3/Part03_27.AVClub/src/main/java/AVClub.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class AVClub {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String text = scanner.nextLine();
+
+            if (text.isEmpty()) {
+                break;
+            }
+
+            String[] words = text.split(" ");
+
+
+            for (String word : words) {
+                if (word.contains("av")) {
+                    System.out.println(word);
+                }
+            }
+        }
+    }
+}
